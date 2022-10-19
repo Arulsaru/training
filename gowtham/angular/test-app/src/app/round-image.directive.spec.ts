13804b29aedc3d5de13bda033a30b88a25da9e38
Gowtham <gowtham.balusamy@trustrace.com>
2022-10-19 22:15:53 +0530

    CRUD

--- gowtham/angular/test-app/src/app/round-image.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RoundImageDirective } from './round-image.directive';
-
-describe('RoundImageDirective', () => {
-  it('should create an instance', () => {
-    const directive = new RoundImageDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- gowtham/angular/test-app/src/app/second.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SecondDirective } from './second.directive';
-
-describe('SecondDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SecondDirective();
-    expect(directive).toBeTruthy();
-  });
-});
